9181eb8f7d728d5306d6664970f2b0963d9bf500
Jeremy Manoto <jeremy@manoto.com.au>
2012-12-13 15:25:09 +1100

    Import Browser Ninja Blocks as yeoman component

--- yeoman/app/scripts/app.js
+++ yeoman/app/scripts/app.js
@@ -10,4 +10,46 @@ var yeomanApp = angular.module('yeomanApp', [])
       .otherwise({
         redirectTo: '/'
       });
+  }])
+  .config(['$locationProvider', function($locationProvider) {
+    $locationProvider.html5Mode(false);
   }]);
+
+  /**
+ * Initialization
+ */
+yeomanApp.run([
+  '$rootScope', '$location', 'UIEvents', 'NinjaService', 'UserService', 'PusherService', 'DeviceService'
+  ,function($rootScope, $location, UIEvents, NinjaService, UserService, PusherService,  DeviceService) {
+
+
+  /**
+   * Global Set Route routine. Used by nav.
+   * Sets the location to the specified route
+   * @param {string} route Location route as specified in $routeProvider
+   */
+  $rootScope.setRoute = function(route) {
+    $location.path(route);
+  };
+
+
+  // Only initialize route change watches once the user status has been determined
+  $rootScope.$on(UIEvents.UserStatusChecked, function() {
+    // console.log("Status Checked");
+    $rootScope.$on('$routeChangeStart', function(eventObj, next, current) {
+      // console.log("route change: ", UserService.IsLoggedIn, next, current);
+    });
+
+
+    $rootScope.$on('$routeChangeSuccess', function(eventObj, current, previous) {
+      // console.log('route success', current, previous);
+    });
+  });
+
+  /**
+   * Automatically get the user login status
+   */
+  UserService.GetLoginStatus();
+
+
+}]);

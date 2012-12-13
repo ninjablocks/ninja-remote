6b59da2c67703f4dc78190b0b7f4fc0e8b210e35
Jeremy Manoto <jeremy@manoto.com.au>
2012-12-13 14:36:48 +1100

    Apply Html5 Mobile Boilerplate

--- yeoman/app/scripts/controllers/main.js
+++ yeoman/app/scripts/controllers/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-yeomanApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
-});
+yeomanApp.controller('MainCtrl'
+  , ['$scope'
+  , function($scope) {
+
+
+
+}]);
